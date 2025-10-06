42ffc11a657293dc5551c654f443446d913398a7
“javayporter” <javayscript@gmail.com>
2025-10-06 11:24:37 -0400

    responsive updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,28 @@
-import React from "react";
 import logo from "../assets/image0.png";
 
-const Navbar: React.FC = () => {
+import "../styles/global.css";
+
+export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">
-        <img src={logo} alt="Flair By Tate Logo" className="navbar-logo" />
+      <div className="navbar-container">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+
+        <ul className="navbar-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
-      <ul className="navLinks">
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
     </nav>
   );
-};
-
-export default Navbar;
+}
